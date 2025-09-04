b00e3a328ea3767bc92c558959801ecb3591307d
timsletap <smit.patel25@protonmail.com>
2025-09-04 14:57:28 -0400

    daily commit

---
+++ LeetCode/FindTheDifference.java
@@ -0,0 +1,21 @@
+public class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        int sSum = 0;
+        int tSum = 0;
+        for(int i = 0; i < s.length(); i++){
+            sSum +=(int) s.charAt(i);
+        }
+         for(int j = 0; j < t.length(); j++){
+            tSum += (int)t.charAt(j);
+        }
+        return (char)(tSum - sSum);
+    }
+    public static void main(String[] args) {
+        FindTheDifference finder = new FindTheDifference();
+        String s = "abcd";
+        String t = "abcde";
+        char difference = finder.findTheDifference(s, t);
+        System.out.println(difference); // Output: 'e'
+    }
+    
+}
---
+++ LeetCode/FirstOccurenceStrinStr.java
@@ -0,0 +1,24 @@
+public class FirstOccurenceStrinStr { 
+
+public int strStr(String haystack, String needle) {
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;        
+    }
+
+    public static void main(String[] args){
+    FirstOccurenceStrinStr finder = new FirstOccurenceStrinStr();
+    String haystack = "hello";
+    String needle = "ll";
+    int index = finder.strStr(haystack, needle);
+    System.out.println(index); // Output: 2
+}
+
+
+
+
+
+}
