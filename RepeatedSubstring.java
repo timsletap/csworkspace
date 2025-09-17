3e0c79ea455be4a2adcdbe7f3526f85e2d60b667
timsletap <smit.patel25@protonmail.com>
2025-09-17 18:01:46 -0400

    daily commir

---
+++ RepeatedSubstring.java
@@ -0,0 +1,13 @@
+public class RepeatedSubstring {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+        String sub = doubled.substring(1, doubled.length() - 1);
+        return sub.contains(s);
+        } 
+    public static void main(String[] args) {
+        RepeatedSubstring rs = new RepeatedSubstring();
+        System.out.println(rs.repeatedSubstringPattern("abab")); // true
+        System.out.println(rs.repeatedSubstringPattern("aba"));  // false
+        System.out.println(rs.repeatedSubstringPattern("abcabcabc")); // true
+    }
+}
